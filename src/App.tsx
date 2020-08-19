b1b75f603218c9c028577b74745b71f1312bceb3
Freyr Þorvaldsson <freyrthorv@gmail.com>
2020-08-19 14:21:16 +0000

    Remove code / assets I don't need, make css naming conventions to my preference

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="front-page">
+			<div className="container">
+				<span className="spooky-boy" role="img" aria-label="Spooky skull">
+					💀
+				</span>
+				<p>select your age and gender</p>
+				{/* TODO: add age and gender selectors */}
+			</div>
+		</div>
+	);
 }
 
 export default App;
